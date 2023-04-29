94aaef22065d5a1f118582d8c36bf3428bfc1e01
Jakub Tokarski <kubatokarski04@gmail.com>
2023-04-29 10:07:19 +0200

    dodanie funkcji GK , ukł2000 , ukł1992

--- Projekt1.py
+++ Projekt1.py
@@ -63,6 +63,30 @@ class Transformacje:
                          [Z - Z0]])
         ENU = R.transpose() @ XYZT
         return (ENU[0][0],ENU[1][0],ENU[2][0])
+    
+    def sigma(self,f):
+        f = radians()
+        A0 = 1 - (self.e2 / 4) - (3 * self.e2**2 / 64) - (5 * self.e2**3 / 256)
+        A2 = (3/8) * (self.e2 + self.e2**2 / 4 + 15 * self.e2**3 / 128)
+        A4 = (15/256) * (self.e2**2 + (3 * self.e2**3 / 4))
+        A6 = 35 * self.e2**3 / 3072
+        sigma = self.a * (A0 * f - A2 * np.sin(2 * f) + A4 * np.sin(4 * f) - A6 * np.sin(6 * f))
+        return(sigma)
+    
+    def XgkYgk(self,f,l,l0):
+       f = radians(f)
+       l = radians(l)
+       l0 = radians(l0)
+       b2 = (self.a**2) * (1 - self.e2) 
+       ep2 = (self.a**2 - b2)
+       dl = l - l0
+       t = np.tan(f)
+       n2 = ep2 * (np.cos(f))**2 
+       N = Transformacje.Np(self,f)
+       sigma0 = sigma(self,f)
+       Xgk = sigma0 + (dl**2 / 2) * N * np.sin(f) * np.cos(f) * (1 + (dl**2 / 12) * (np.cos(f))**2 * (5 - t**2 + 9 * n2 + 4 * n2**2) + (dl**4 / 360) * (np.cos(f))**4 * (61 - 58 * t**2 + t**4 + 270 * n2 - 330 * n2 * t**2))
+       Ygk = dl * N * np.cos(f) * (1 + (dl**2 / 6) * (np.cos(f))**2 * (1 - t**2 + n2) + (dl**4 / 120) * (np.cos(f))**4 * (5 - 18 * t**2 + t**4 + 14 * n2 - 58 * n2 * t**2))
+       return(Xgk,Ygk)
 
 geo = Transformacje(model = "wgs84")
 # dane XYZ geocentryczne
