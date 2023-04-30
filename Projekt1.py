8db5e6f91f6e14260ac8c2b85869dd066907aa46
Jakub Tokarski <kubatokarski04@gmail.com>
2023-04-30 19:55:42 +0200

    częściowe sprawdzenie działania funkcji

--- Projekt1.py
+++ Projekt1.py
@@ -1,3 +1,4 @@
+import math as m
 from math import sin, cos, sqrt, atan, atan2, degrees, radians
 import numpy as np
 
@@ -40,7 +41,7 @@ class Transformacje:
         self.e2 = (2 * self.flat - self.flat ** 2) 
         
         
-    def Np(self,fi):
+    def Np(self,flh, jedn = "dec"):
         """
         Największy promień krzywizny na pozycję uzytkownika
         
@@ -53,145 +54,330 @@ class Transformacje:
         N : float
             [metry] - największy promień krzywizny
         """
-        N = self.a / np.sqrt(1 - self.e2 * np.sin(fi)**2)
+        if jedn == 'rad':
+            pass
+        elif jedn == 'dec':
+            flh = [float(np.radians(flh[0])),float(np.radians(flh[1])),float(flh[2])]
+        elif jedn == 'gra':
+            flh = [float(flh[0]*m.pi/200), float(flh[1]*m.pi/200), float(flh[2])]
+        else:
+            raise NotImplementedError(f"{jedn} nie jest w zbiorze okreslen")
+        N = self.a / np.sqrt(1 - self.e2 * np.sin(flh[0])**2)
         return(N)
 
-    def xyz2flh(self,X,Y,Z):
+    def xyz2flh(self, xyz, jedn = 'dec'):
         """
         Algorytm Hirvonena – algorytm służący do transformacji współrzędnych ortokartezjańskich (prostokątnych) x, y, z na współrzędne geodezyjne B, L, h.
         Jest to proces iteracyjny. W wyniku 3-4-krotnego powtarzania procedury można przeliczyć współrzędne na poziomie dokładności 1 cm.
         
         Parameters
         ----------
-        X,Y,Z : float
+        xyz : [list]
             [metry] - współrzędne w układzie orto-kartezjańskim
+        jedn : STR, optional
+           Jednostka podawanych wartosci. The default is 'dec'.
+           ["rad" - radiany, "gra" - grady, "dec" - stopnie]
+        
+        Raises
+        ------
+        NotImplementedError
+        Jezeli podana jednostka jest poza zbiorem.
+       
         Returns
         -------
+        flh = [f,l,h]
         f : float
-            [stopnie dziesiętne] - szerokość geodezyjna
+            - szerokość geodezyjna
         l : float
-            [stopnie dziesiętne] - długość geodezyjna
+            - długość geodezyjna
         h : float
             [metry] - wysokość geometryczna(elipsoidalna)
         """
+        X = xyz[0]
+        Y = xyz[1]
+        Z = xyz[2]
         p = np.sqrt(X**2 + Y**2)
         f = np.arctan(Z/p * (1-self.e2))
         while True:
-            N = Transformacje.Np(self,f)
+            N = Transformacje.Np(self, [f] ,'rad')
             h= ( p / np.cos(f)) - N
             fp = f
             f=np.arctan(Z/(p*(1-self.e2 * N / (N+h))))
             if abs(fp-f)<(0.000001/206265):
                 break
         l=np.arctan2(Y,X)
-        return(f,l,h) 
+        flh = [f,l,h]
+        if jedn == 'rad':
+            pass
+        elif jedn == 'dec':
+            flh = [float(np.degrees(f)),float(np.degrees(l)),float(h)]
+        elif jedn == 'gra':
+            flh = [float(f*200/m.pi), float(l*200/m.pi), float(h)]
+        else:
+            raise NotImplementedError(f"{jedn} nie jest w zbiorze okreslen")
+        return (flh) 
    
-    def flh2XYZ(self,fi,la,h):
+    def flh2XYZ(self, flh, jedn = 'dec'):
         """
+        Funkcja transformujaca współrzędne geodezyjne długość szerokość i wysokośc elipsoidalna 
+        na współrzędne ortokartezjańskie
+        
         Parameters
         ----------
-        f : float
-            [stopnie dziesiętne] - szerokość geodezyjna
-        l : float
-            [stopnie dziesiętne] - długość geodezyjna
-        h : float
-            [metry] - wysokość geometryczna(elipsoidalna)
+        flh - wspolrzedne geodezyjne
+        jedn : [str] , optional
+             Jednostka podawanych wartosci. The default is 'dec'.
+             ["rad" - radiany, "gra" - grady, "dec" - stopnie]
+        
+        Raises
+        ------
+        NotImplementedError
+        Jezeli podana jednostka jest poza zbiorem.
+       
         Returns
         -------
         X,Y,Z : float
             [metry] - współrzędne w układzie orto-kartezjańskim
         """
-        N = Transformacje.Np(self,fi)
-        X = (N + h) * np.cos(fi) * np.cos(la)
-        Y = (N + h) * np.cos(fi) * np.sin(la)
-        Z =(N*(1-self.e2)+h) * np.sin(fi) 
-        return(X,Y,Z)
+        
+        if jedn == 'rad':
+            pass
+        elif jedn == 'dec':
+            flh = [float(np.radians(flh[0])),float(np.radians(flh[1])),float(flh[2])]
+        elif jedn == 'gra':
+            flh = [float(flh[0]*m.pi/200), float(flh[1]*m.pi/200), float(flh[2])]
+        else:
+            raise NotImplementedError(f"{jedn} nie jest w zbiorze okreslen")
+        N = Transformacje.Np(self,flh[0])
+        X = (N + flh[2]) * np.cos(flh[0]) * np.cos(flh[1])
+        Y = (N + flh[2]) * np.cos(flh[0]) * np.sin(flh[1])
+        Z =(N*(1-self.e2)+flh[2]) * np.sin(flh[0]) 
+        xyz = [round(X,3),round(Y,3),round(Z,3)]
+        return(xyz)
     
-    def xyz2neu(self,X0,Y0,Z0,X,Y,Z):
+    def xyz2neu(self, xyz0, xyz, jedn = 'dec'):
         """
         Sferyczny układ współrzędnych – układ współrzędnych w trójwymiarowej przestrzeni euklidesowej.
         Parameters
         ----------
-        X0,Y0,Z0 : float
+        xyz0 : [list]
             [metry] - współrzędne punktu w układzie orto-kartezjańskim
-        X,Y,Z : float
+        xyz : [list]
             [metry] - współrzędne referencyjne w układzie orto-kartezjańskim
+        jedn : [str] , optional
+             Jednostka podawanych wartosci. The default is 'dec'.
+             ["rad" - radiany, "gra" - grady, "dec" - stopnie]
+        
+        Raises
+        ------
+        NotImplementedError
+        Jezeli podana jednostka jest poza zbiorem.
+       
         Returns
         -------
-        N,E,U : float
+        neu : [list]
             [metry] - współrzędne w układzie sferycznym
         """
-        fi,la,ha = Transformacje.xyz2flh(self, X0, Y0, Z0)
-        R = np.array([[-np.sin(radians(la)) , -np.sin(radians(fi)) * np.cos(radians(la)) , np.cos(radians(fi)) * np.cos(radians(la))],
-             [np.cos(radians(fi))  , -np.sin(radians(fi)) * np.sin(radians(la)) , np.cos(radians(fi)) * np.sin(radians(la))],
-             [0 , np.cos(fi) , np.sin(fi)]])
-        XYZT = np.array([[X - X0],
-                         [Y - Y0],
-                         [Z - Z0]])
+        flh = Transformacje.xyz2flh(self,xyz0)
+        if jedn == 'rad':
+            pass
+        elif jedn == 'dec':
+            flh = [float(np.radians(flh[0])),float(np.radians(flh[1])),float(flh[2])]
+        elif jedn == 'gra':
+            flh = [float(flh[0]*m.pi/200), float(flh[1]*m.pi/200), float(flh[2])]
+        else:
+            raise NotImplementedError(f"{jedn} nie jest w zbiorze okreslen")
+        R = np.array([[-np.sin(radians(flh[1])) , -np.sin(radians(flh[0])) * np.cos(radians(flh[1])) , np.cos(radians(flh[0])) * np.cos(radians(flh[1]))],
+             [np.cos(radians(flh[0]))  , -np.sin(radians(flh[0])) * np.sin(radians(flh[1])) , np.cos(radians(flh[0])) * np.sin(radians(flh[1]))],
+             [0 , np.cos(flh[0]) , np.sin(flh[0])]])
+        XYZT = np.array([[xyz[0] - xyz0[0]],
+                         [xyz[1] - xyz0[1]],
+                         [xyz[2] - xyz0[2]]])
         ENU = R.transpose() @ XYZT
-        return (ENU[0][0],ENU[1][0],ENU[2][0])
+        enu = [round(ENU[0][0],3), round(ENU[1][0],3), round(ENU[2][0],3)]
+        return(enu)
     
-    def sigma(self,f):
-        f = radians()
+    def sigma(self, flh, jedn = "dec"):
+        '''
+        Algorytm liczący długosć łuku południka.
+
+        Parameters
+        ----------
+        flh : LIST
+            wspolrzedne geodezyjne phi, lam, h [metry]
+        jedn : STR, optional
+            Jednostka wspolrzednych geodezyjnych. Domyslna jest "dec".
+            ["rad" - radiany, "gra" - grady, "dec" - stopnie]
+
+        Raises
+        ------
+        NotImplementedError
+            Jezeli podana jednostka jest poza zbiorem.
+
+        Returns
+        -------
+        sigma : FLOAT
+            Dlugosc luku poludnika [metry].
+
+        '''
+        
+        if jedn == 'rad':
+            pass
+        elif jedn == 'dec':
+            flh = [float(np.radians(flh[0])),float(np.radians(flh[1])),float(flh[2])]
+        elif jedn == 'gra':
+            flh = [float(flh[0]*m.pi/200), float(flh[1]*m.pi/200), float(flh[2])]
+        else:
+            raise NotImplementedError(f"{jedn} nie jest w zbiorze okreslen")
+        
         A0 = 1 - (self.e2 / 4) - (3 * self.e2**2 / 64) - (5 * self.e2**3 / 256)
         A2 = (3/8) * (self.e2 + self.e2**2 / 4 + 15 * self.e2**3 / 128)
         A4 = (15/256) * (self.e2**2 + (3 * self.e2**3 / 4))
         A6 = 35 * self.e2**3 / 3072
-        sigma = self.a * (A0 * f - A2 * np.sin(2 * f) + A4 * np.sin(4 * f) - A6 * np.sin(6 * f))
+        sigma = self.a * (A0 * flh[0] - A2 * np.sin(2 * flh[0]) + A4 * np.sin(4 * flh[0]) - A6 * np.sin(6 * flh[0]))
         return(sigma)
     
-    def XgkYgk(self,f,l,l0):
+    def XgkYgk(self, flh, l0, jedn = 'dec'):
         """
         Odwzorowanie Gaussa-Krügera – odwzorowanie kartograficzne pasów południkowych na pobocznicę walca stycznego
         do południka środkowego (osiowego) każdego odwzorowywanego pasa.
         Jest to wiernokątne, walcowe, poprzeczne odwzorowanie elipsoidy, w którym każdy pas odwzorowuje się oddzielnie.
+        
         Parameters
         ----------
-        f : float
-            [stopnie dziesiętne] - szerokość geodezyjna
-        l : float
-            [stopnie dziesiętne] - długość geodezyjna
-        l0 : float
+        flh : LIST
+            wspolrzedne geodezyjne phi, lam, h [metry]
+        l0 : int
             [stopnie dziesiętne] - południk osiowy
+        jedn : STR, optional
+            Jednostka wspolrzednych geodezyjnych. Domyslna jest "dec".
+            ["rad" - radiany, "gra" - grady, "dec" - stopnie]
+
+        Raises
+        ------
+        NotImplementedError
+            Jezeli podana jednostka jest poza zbiorem.
+
         Returns
         -------
-        Xgk : float
-            [metry] - Współrzędna x w układzie Gaussa-Krugera
-        Ygk : float
-            [metry] - Współrzędna y w układzie Gaussa-Krugera
+        xygk : LIST
+            Wspolrzedne odwzorowania Gaussa-Krugera [metry]
         """
-        f = radians(f)
-        l = radians(l)
+        
+        if jedn == 'rad':
+            pass
+        elif jedn == 'dec':
+            flh = [float(np.radians(flh[0])),float(np.radians(flh[1])),float(flh[2])]
+        elif jedn == 'gra':
+            flh = [float(flh[0]*m.pi/200), float(flh[1]*m.pi/200), float(flh[2])]
+        else:
+            raise NotImplementedError(f"{jedn} nie jest w zbiorze okreslen")
+        
         l0 = radians(l0)
         b2 = (self.a**2) * (1 - self.e2) 
         ep2 = (self.a**2 - b2)
-        dl = l - l0
-        t = np.tan(f)
-        n2 = ep2 * (np.cos(f))**2 
-        N = Transformacje.Np(self,f)
-        sigma0 = Transformacje.sigma(self,f)
-        Xgk = sigma0 + (dl**2 / 2) * N * np.sin(f) * np.cos(f) * (1 + (dl**2 / 12) * (np.cos(f))**2 * (5 - t**2 + 9 * n2 + 4 * n2**2) + (dl**4 / 360) * (np.cos(f))**4 * (61 - 58 * t**2 + t**4 + 270 * n2 - 330 * n2 * t**2))
-        Ygk = dl * N * np.cos(f) * (1 + (dl**2 / 6) * (np.cos(f))**2 * (1 - t**2 + n2) + (dl**4 / 120) * (np.cos(f))**4 * (5 - 18 * t**2 + t**4 + 14 * n2 - 58 * n2 * t**2))
-        return(Xgk,Ygk)
+        dl = flh[1] - l0
+        t = np.tan(flh[0])
+        n2 = ep2 * (np.cos(flh[0]))**2 
+        N = Transformacje.Np(self,flh[0])
+        sigma0 = Transformacje.sigma(self,flh[0])
+        Xgk = sigma0 + (dl**2 / 2) * N * np.sin(flh[0]) * np.cos(flh[0]) * (1 + (dl**2 / 12) * (np.cos(flh[0]))**2 * (5 - t**2 + 9 * n2 + 4 * n2**2) + (dl**4 / 360) * (np.cos(flh[0]))**4 * (61 - 58 * t**2 + t**4 + 270 * n2 - 330 * n2 * t**2))
+        Ygk = dl * N * np.cos(flh[0]) * (1 + (dl**2 / 6) * (np.cos(flh[0]))**2 * (1 - t**2 + n2) + (dl**4 / 120) * (np.cos(flh[0]))**4 * (5 - 18 * t**2 + t**4 + 14 * n2 - 58 * n2 * t**2))
+        xygk = [Xgk, Ygk]
+        return(xygk)
 
-    def XY2000(self,fi,la,l0):
-        Xgk,Ygk = Transformacje.XgkYgk(self, fi, la, l0)
-        X2000 = Xgk * 0.999923 
-        Y2000 = Ygk * 0.999923 + l0/3 * 1000000 + 500000
-        return(X2000,Y2000)
+    def XY2000(self, flh, l0, jedn = 'dec'):
+        """
+        Układ współrzędnych 2000 – układ współrzędnych płaskich prostokątnych zwany układem „2000”, 
+        powstały w wyniku zastosowania odwzorowania Gaussa-Krügera dla elipsoidy GRS 80 w czterech trzystopniowych strefach 
+        o południkach osiowych 15°E, 18°E, 21°E i 24°E, oznaczone odpowiednio numerami – 5, 6, 7 i 8.
+       
+        Parameters
+        ----------
+        flh : LIST
+            wspolrzedne geodezyjne phi, lam, h [metry]
+        l0 : int
+            [stopnie dziesiętne] - południk osiowy
+        jedn : STR, optional
+            Jednostka wspolrzednych geodezyjnych. Domyslna jest "dec".
+            ["rad" - radiany, "gra" - grady, "dec" - stopnie]
 
-    def XY1992(self,fi,la):
-        Xgk,Ygk = Transformacje.XgkYgk(self, fi, la, 19)
-        X1992 = Xgk * 0.9993 - 5300000
-        Y1992 = Ygk * 0.9993 + 500000
-        return(X1992,Y1992)
+        Raises
+        ------
+        NotImplementedError
+            Jezeli podana jednostka jest poza zbiorem.
+
+        Returns
+        -------
+        xy2000 : LIST
+            Wspolrzedne w układzie PL-2000 [metry]
+        """
+        
+        if jedn == 'rad':
+            pass
+        elif jedn == 'dec':
+            flh = [float(np.radians(flh[0])),float(np.radians(flh[1])),float(flh[2])]
+        elif jedn == 'gra':
+            flh = [float(flh[0]*m.pi/200), float(flh[1]*m.pi/200), float(flh[2])]
+        else:
+            raise NotImplementedError(f"{jedn} nie jest w zbiorze okreslen")
+       
+        xygk = Transformacje.XgkYgk(self, flh, l0)
+        X2000 = xygk[0] * 0.999923 
+        Y2000 = xygk[1] * 0.999923 + l0/3 * 1000000 + 500000
+        xy2000 = [X2000,Y2000]
+        return(xy2000)
+
+    def XY1992(self, flh, jedn = 'dec'):
+        """
+        Układ współrzędnych 1992 – układ współrzędnych płaskich prostokątnych oparty na odwzorowaniu Gaussa-Krügera dla elipsoidy GRS80 w jednej dziesięciostopniowej strefie.
+        Początkiem układu jest punkt przecięcia południka 19°E z obrazem równika.
+        
+        Parameters
+        ----------
+        flh : LIST
+            wspolrzedne geodezyjne phi, lam, h [metry]
+        l0 : int
+            [stopnie dziesiętne] - południk osiowy
+        jedn : STR, optional
+            Jednostka wspolrzednych geodezyjnych. Domyslna jest "dec".
+            ["rad" - radiany, "gra" - grady, "dec" - stopnie]
+
+        Raises
+        ------
+        NotImplementedError
+            Jezeli podana jednostka jest poza zbiorem.
+
+        Returns
+        -------
+        xy1992 : LIST
+            Wspolrzedne w układzie PL-1992 [metry]
+        """
+        if jedn == 'rad':
+            pass
+        elif jedn == 'dec':
+            flh = [float(np.radians(flh[0])),float(np.radians(flh[1])),float(flh[2])]
+        elif jedn == 'gra':
+            flh = [float(flh[0]*m.pi/200), float(flh[1]*m.pi/200), float(flh[2])]
+        else:
+            raise NotImplementedError(f"{jedn} nie jest w zbiorze okreslen")
+       
+        xygk = Transformacje.XgkYgk(self, flh, 19)
+        X1992 = xygk[0] * 0.9993 - 5300000
+        Y1992 = xygk[1] * 0.9993 + 500000
+        xy1992 = [X1992,Y1992]
+        return(xy1992)
        
         
 geo = Transformacje(model = "wgs84")
 # dane XYZ geocentryczne
 X = 3664940.500; Y = 1409153.590; Z = 5009571.170
-phi, lam, h = geo.xyz2flh(X, Y, Z)
-print(phi, lam, h)
-E,N,U = geo.xyz2neu(X,Y,Z,(X + 1000),(Y + 2000),(Z + 3000))
-print(E,N,U)
-
+xyz = [X,Y,Z]
+flh = geo.xyz2flh(xyz,'dec')
+print(flh)
+xyz0 = [X+100,Y+1000,Z+234]
+neu = geo.xyz2neu(xyz0, xyz,'dec')
+print(neu)
+l0 = 21
+xy00 = geo.XY2000(flh, l0, jedn = 'dec')
+print(xy00)
